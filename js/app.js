57884c2f6da95cdfe179c6306d055c179a7613c5
Gabriel e Giuseppe Dupla Ti <gabrielegiuseppeduplati@gmail.com>
2026-03-02 11:27:08 -0300

    02 - Operadores relacionais e lógicos com Javascript

--- js/app.js
+++ js/app.js
@@ -1,5 +1,5 @@
-let primeiroNumero = parseInt(Math.random()*10000);
-let segundoNumero = parseInt(Math.random()*500);
+let primeiroNumero = parseInt(Math.random()*10);
+let segundoNumero = parseInt(Math.random()*10);
 let resultado =primeiroNumero + segundoNumero;
 console.log(primeiroNumero,segundoNumero);
 console.log(resultado);
@@ -14,3 +14,57 @@ document.getElementById('elSegundoNumero').textContent =
 
 document.getElementById('resultadoSoma').textContent = 
 `${primeiroNumero} + ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero - segundoNumero;
+document.getElementById('resultadoSubtracao').textContent = 
+`${primeiroNumero} - ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero * segundoNumero;
+document.getElementById('resultadoMultiplicacao').textContent = 
+`${primeiroNumero} * ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero / segundoNumero;
+document.getElementById('resultadoDivisao').textContent = 
+`${primeiroNumero} / ${segundoNumero} = ${resultado.toFixed(1)}`;
+
+resultado =primeiroNumero % segundoNumero;
+document.getElementById('resultadoModulo').textContent = 
+`${primeiroNumero} % ${segundoNumero} = ${resultado.toFixed(1)}`;
+
+resultado =primeiroNumero === segundoNumero;
+document.getElementById('igualdade').textContent = 
+`${primeiroNumero} === ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero !== segundoNumero;
+document.getElementById('desigualdade').textContent = 
+`${primeiroNumero} !== ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero >= segundoNumero;
+document.getElementById('maiorIgual').textContent = 
+`${primeiroNumero} >= ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero <= segundoNumero;
+document.getElementById('menorIgual').textContent = 
+`${primeiroNumero} <= ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero > segundoNumero;
+document.getElementById('maior').textContent = 
+`${primeiroNumero} > ${segundoNumero} = ${resultado}`;
+
+resultado =primeiroNumero < segundoNumero;
+document.getElementById('menor').textContent = 
+`${primeiroNumero} < ${segundoNumero} = ${resultado}`;
+
+resultado = (primeiroNumero >= segundoNumero) || ( segundoNumero > 3);
+document.getElementById('operadorOU').textContent =
+`${primeiroNumero} > ${segundoNumero} || ${segundoNumero} >= 3 = ${resultado}`;
+
+resultado = (primeiroNumero >= segundoNumero) && ( segundoNumero > 3);
+document.getElementById('operadorE').textContent =
+`${primeiroNumero} > ${segundoNumero} && ${segundoNumero} >= 3 = ${resultado}`;
+
+
+
+resultado = !((primeiroNumero >= segundoNumero) && ( segundoNumero > 3));
+document.getElementById('operadorNAO').textContent =
+`!${primeiroNumero} >= ${segundoNumero} && ${segundoNumero} >= 3) = ${resultado}`;
--- readme.md
+++ readme.md
@@ -1,1 +1,3 @@
 aula 02 de JS - Sorteando número e utilizando operadores aritméticos
+
+git comit -m "02 - Operadores relacionais e lógicos com Javascript"
